ce6f6eafd6a3693f96a4d4cd745363f8ed5bc4dd
Leonardo Kremer <devleonardok@gmail.com>
2026-04-07 20:26:37 -0300

    feat: add environment settings class using Pydantic BaseSettings

--- initial_pyspark/projects/job_salary_dataset/src/config.py
+++ initial_pyspark/projects/job_salary_dataset/src/config.py
@@ -2,6 +2,20 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
+#Pydantic
+from pydantic_settings import BaseSettings,SettingsConfigDict
+from pydantic import SecretStr, Field
 
 #Spark create
 spark = SparkSession.builder.master("local[*]").appName("spark_project_job_salary").getOrCreate()
+
+#Environments
+class Environment(BaseSettings):
+    model_config = SettingsConfigDict(env_file="../.env", extra="ignore")
+    database_url : SecretStr = Field(..., alias="DATABASE_ADDRESS")
+    database_username : SecretStr = Field(..., alias="DATABASE_USER")
+    database_password : SecretStr = Field(..., alias="DATABASE_PASSWORD")
+    database_name : SecretStr = Field(..., alias="DATABASE_NAME")
+    
+
+env_vars = Environment()
